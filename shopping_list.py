85c46a94e644b3c43d543d21187060720809b185
pogmac <pogorzelski.maciej@gmail.com>
2023-04-23 13:19:41 +0200

    FIX minor changes in shopping_list.py file

--- shopping_list.py
+++ shopping_list.py
@@ -12,7 +12,8 @@ for k, v in shopping_list.items():
     print(f"I'm going to the {k.capitalize()} and buying ", end ="")
     listlen +=len(v)
     for i in range(len(v)):
-        print(v[i].capitalize(), end = " ")
+        print(shopping_list[k][i].capitalize(), end = " ")
     print("")
+    
 print(f"\nI've bought {listlen} items")
         
